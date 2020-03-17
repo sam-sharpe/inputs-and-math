7c621e9ca070c98f5c3bb7d4e078e2a7abe88a2b
Sam Sharpe <samsharpe@Powerbook-G4.local>
2020-03-17 18:59:07 -0400

    completed calculator js

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 $("body").on("keyup keydown keypress change", ".item input", function(e){
-  let total = 0;
+  let pre_tariff_total_sum = 0;
+  let post_tariff_total_sum = 0;
 
 //finds each item within a table row
   $("tr.item").each(function () {
@@ -19,17 +20,45 @@ $("body").on("keyup keydown keypress change", ".item input", function(e){
 //and make it a percentage instead of a whole number
     pct = pct / 100;
 
+//add the if statement – **BEFORE THE FLOATS** –for the function to skip rows without an input. this prevents the HTML from throwing a NaN
+if(qty === "" || cost === "") {
+  return false;
+}
+
 //make them into numbers. we want floats because the input might not always be a whole #
     qty = parseFloat(qty);
     cost = parseFloat(cost);
-
-
 
     const pre_tariff_total = qty * cost;
 
     const post_tariff_total = (pre_tariff_total + (pre_tariff_total * pct));
 
+    pre_tariff_total_sum = pre_tariff_total_sum + pre_tariff_total;
+    post_tariff_total_sum = post_tariff_total_sum + post_tariff_total;
+
+    console.log("pre tarriff total");
     console.log(pre_tariff_total);
+
+//insert the pre-total into each td's span
+    $this_row.find("td.pre_total span").text(pre_tariff_total);
+    //insert the post-total into each td's span
+    $this_row.find("td.post_total span").text(post_tariff_total);
+
+    console.log("post tarriff total");
     console.log(post_tariff_total);
-  });
-});
+
+  }); //THIS ENDS THE EACH LOOP
+
+    console.log("pre tarriff total sum");
+    console.log(pre_tariff_total_sum);
+
+    //insert sum total into span below
+    $("#pre_total span").text(pre_tariff_total_sum);
+
+    console.log("post tarriff total sum");
+    console.log(post_tariff_total_sum);
+
+    //insert sum total into span below
+    $("#post_total span").text(post_tariff_total_sum);
+
+}); //THIS ENDS THE KEYPRESS LISTENER
